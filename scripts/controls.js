b4a0937f76a3ab43451747e8a2a6cb05243b9546
Andrew Rasmussen <andrew.rasmussen1@aggiemail.usu.edu>
2022-02-25 14:55:46 -0700

    control settings page

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,14 +1,94 @@
 MyGame.screens['controls'] = (function(game) {
     'use strict';
 
+    let selectedControl = null;
+
+    let keyListener = null;
+
+    let controls = {
+        down: {
+            element: document.getElementById('control-down'),
+            value: 'ArrowDown',
+        },
+        up: {
+            element: document.getElementById('control-up'),
+            value: 'ArrowUp',
+        },
+        right: {
+            element: document.getElementById("control-right"),
+            value: 'ArrowRight'
+        },
+        left: {
+            element: document.getElementById('control-left'),
+            value: 'ArrowLeft'
+        },
+        fire: {
+            element: document.getElementById('control-fire'),
+            value: 'Space'
+        }
+    }
+
+
     function initialize() {
-        document.getElementById('controls-to-main').addEventListener('click', () => {
-            game.showScreen('main-menu')
-        })
+        //
+    }
+
+    const styleSelected = (selected) => {
+        console.log('style selected called')
+        for(let control of Object.keys(controls)) {
+            if(control === selected) {
+                controls[control].element.classList.add('selected')
+            } else {
+                controls[control].element.classList.remove('selected')
+            }
+        }
+    }
+
+    const setControlHtml = (control, value) => {
+        controls[control].element.innerHTML = value
+    }
+
+    const codeInUse = (code) => {
+        for(let control of Object.keys(controls)) {
+            if(controls[control].value === code) {
+                return true
+            }
+        }
+        return false
+    }
+
+    const storeControl = (control, value) => {
+        localStorage.setItem(control, value)
     }
     
     function run() {
-        // nothing to run
+        document.getElementById('controls-to-main').addEventListener('click', () => {
+            game.showScreen('main-menu')
+            document.removeEventListener('keydown', keyListener);
+            selectedControl = null;
+            styleSelected('')
+        })
+
+        for(let control of Object.keys(controls)) {
+            let storedControl = localStorage.getItem(control);
+            if(storedControl !== null) {
+                controls[control].value = storedControl;
+            }
+            setControlHtml(control, controls[control].value)
+            controls[control].element.parentNode.addEventListener('click', () => {
+                selectedControl = control;
+                styleSelected(control);
+            })
+        }
+
+        keyListener = document.addEventListener("keydown", e => {
+            if(selectedControl !== null && !codeInUse(e.code)) {
+                controls[selectedControl].value = e.code
+                setControlHtml(selectedControl, e.code)
+                storeControl(selectedControl, e.code)
+            }
+        });
+
     }
     
     return {
